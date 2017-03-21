da63fab19b2cb24c7747e8df630afbc4414d4e41
Gena <gena@e64.net>
2017-03-21 21:56:10 +0100

    exercice3

---
+++ exercice-fonction3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP</title>
+</head>
+<body>
+	<?php
+	function exercice3($texte1, $texte2){
+		return($texte1.$texte2);
+	}
+	
+	echo exercice3('texte1' , 'texte2');
+	?>
+	
+</body>
+</html>
