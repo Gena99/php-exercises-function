2d234e318abc9f4b5aa17fb66d1eeda04d294359
Gena <gena@e64.net>
2017-03-21 22:42:21 +0100

    exercice5

---
+++ exercice-fonction5.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP</title>
+</head>
+<body>
+	<?php
+
+	function exercice5($nombre, $texte){
+		return ($nombre . $texte);
+
+	}
+	echo exercice5(6, '<br/> coucou');
+
+
+
+
+
+	?>
+
+</body>
+</html>
