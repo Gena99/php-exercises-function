a0a3526ebe44f70f13e7294593a8ad5b92ac4163
Gena <gena@e64.net>
2017-03-21 21:35:56 +0100

    exercice1

---
+++ exercice-fonction1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP</title>
+</head>
+<body>
+	<?php
+	function exercice1(){
+		return (true);
+	}
+	echo exercice1();
+	?>
+	
+</body>
+</html>
