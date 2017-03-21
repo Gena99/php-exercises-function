7db9d065847776ea353e20d5d83ef16f574f0e71
Gena <gena@e64.net>
2017-03-21 21:46:30 +0100

    exercice2

---
+++ exercice-fonction2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP</title>
+</head>
+<body>
+	<?php
+	function exercice2 ($texte){
+		return ($texte); 
+
+	}
+	echo exercice2('ok');
+
+	?>
+	
+</body>
+</html>
